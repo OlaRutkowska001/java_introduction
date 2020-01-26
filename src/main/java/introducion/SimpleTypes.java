1bd96bf738aff8adc9d3038560023415a1821503
Ola Rutkowska <rutkowska_ola@wp.pl>
2020-01-26 12:16:06 +0100

    Created my first java aplication

---
+++ src/main/java/introducion/SimpleTypes.java
@@ -0,0 +1,13 @@
+package introducion;
+
+public class SimpleTypes {
+
+    public static void main(String[] args) {
+        int age;
+        age = 0;
+        System.out.println("Wiek to" + age);
+
+        long lottoReward = 32_000_000L;
+    }
+
+}
