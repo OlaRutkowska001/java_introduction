b8600d7127360b8cf7ad2114288692dad63bfd22
Ola Rutkowska <rutkowska_ola@wp.pl>
2020-01-26 12:54:49 +0100

    Crearted exercise for simple types.

--- src/main/java/introducion/SimpleTypes.java
+++ src/main/java/introducion/SimpleTypes.java
@@ -8,6 +8,23 @@ public class SimpleTypes {
         System.out.println("Wiek to" + age);
 
         long lottoReward = 32_000_000L;
+        System.out.println("Wygrana w lotto to " + lottoReward);
+
+        short dayOfMonth = 29;
+        dayOfMonth = 23;
+        System.out.println("Dzień miesiąca to " + dayOfMonth);
+
+        final float PI = 3.14F;
+        //FINALNA wartość pozostaje stała
+        System.out.println("Liczba PI to " + PI);
+
+        double distans = 200D;
+        System.out.println("Dystans wynosi " + distans);
+
+        boolean isPaid = false;
+        System.out.println("Czy zapłacono rachunek? " + isPaid);
+        isPaid = true;
+        System.out.println("A może teraz zapłącono? " + isPaid);
     }
 
 }
