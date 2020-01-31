02c3c77f5e27fc091b9c30c4d7ad01ff0c91e34b
Ola Rutkowska <olarutkowska001@gmail.com>
2020-01-31 21:59:38 +0100

    This is my simple types

---
+++ src/main/java/introducion/MySimpleTypes.java
@@ -0,0 +1,64 @@
+package introducion;
+
+import jdk.swing.interop.SwingInterOpUtils;
+import org.apache.commons.io.output.AppendableOutputStream;
+
+public class MySimpleTypes {
+
+
+    public static void main(String[] args) {
+        System.out.println("Value range int: ");
+        //min and max vaiue int
+        System.out.println("Min value int is " + Integer.MIN_VALUE);
+        System.out.println("Max value int is " + Integer.MAX_VALUE);
+        int myVar = 30;
+
+        System.out.println(
+
+        );
+        System.out.println("Value range long: ");
+    //min and max value
+        System.out.println("Min value long is " + Long.MIN_VALUE);
+        System.out.println("Max value long is " + Long.MAX_VALUE);
+        Long myVar1 = 1000L;
+
+        System.out.println(
+
+        );
+        System.out.println("Value range double: "
+        );
+    //min and max double
+        System.out.println("Min value double is " + Double.MIN_VALUE);
+        System.out.println("Max value double is " + Double.MAX_VALUE);
+        Double myVar2 = 23476655D;
+        System.out.println(
+
+        );
+        System.out.println("Value range short: ");
+        //min i max short
+
+        System.out.println("Min value short is " + Short.MIN_VALUE);
+        System.out.println("Max value short is " + Short.MAX_VALUE);
+        short myVar3 = 7;
+
+        int number;
+        number = Short.MIN_VALUE - 1;
+        System.out.println(number);
+        number = Short.MAX_VALUE + 1;
+        System.out.println(number);
+
+        System.out.println(
+
+        );
+        System.out.println("Value range float is: ");
+        //min and max float
+        System.out.println("Min value float is " + Float.MIN_VALUE);
+        System.out.println("Max value float is " + Float.MAX_VALUE);
+        float myValue = 6536424F;
+
+
+    }
+
+
+
+    }
