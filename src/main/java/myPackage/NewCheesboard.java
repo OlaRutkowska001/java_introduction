24ea476e0ac8c2181f94cf25050470715c9db490
Ola Rutkowska <olarutkowska001@gmail.com>
2020-02-06 12:35:26 +0100

    My commit newcheesboard :)

---
+++ src/main/java/myPackage/NewCheesboard.java
@@ -0,0 +1,29 @@
+package myPackage;
+
+import java.util.Scanner;
+
+public class NewCheesboard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wartość wierszy ");
+        int number = scanner.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                if((i%2==0 && j%2 == 1) || (i%2==1 && j%2==0)) {
+                    System.out.print("##");
+                }else {
+                    System.out.print("**");
+
+                }
+            }
+                    System.out.println();
+                }
+
+            }
+
+        }
+
+
+
--- src/main/java/myPackage/TwoCheesboard.java
+++ src/main/java/myPackage/TwoCheesboard.java
@@ -10,13 +10,9 @@ public class TwoCheesboard {
             int number = scanner.nextInt();
 
             System.out.println("Wielkość szachownicy wynosi " + number + " x " + number);
-            int i = 0;
-            System.out.println(number + "╔");
-
-            System.out.println("═");
 
 
-            for (i = 0; i < number; i++) {
+            for (int i = 0; i < number; i++) {
                 for (int j = 0; j < number; j++) {
                     if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0)) {
                         System.out.print("#");
@@ -24,7 +20,7 @@ public class TwoCheesboard {
                         System.out.print("*");
                     }
                 }
-                System.out.println();
+               // System.out.println();
             }
         }
 
